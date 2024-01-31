a8254ca093fc1839649e634df75df4bbf4d8f042
Michael Green <michaelgreen06@gmail.com>
2024-01-31 11:22:34 +0700

    converted dispos to csv. Prepping to create table 1

---
+++ convert2csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+input_directory = '/Users/michaelgreen/udemy/db-proj/data/us smoke shops/xlsx'
+# Define your output directory here
+output_directory = '/Users/michaelgreen/udemy/db-proj/data/us smoke shops/csv'
+
+for filename in os.listdir(input_directory):
+    if filename.endswith('.xlsx'):
+        input_filepath = os.path.join(input_directory, filename)
+        df = pd.read_excel(input_filepath)
+
+        # Construct the output filepath and replace the extension
+        output_filename = filename.replace('.xlsx', '.csv')
+        output_filepath = os.path.join(output_directory, output_filename)
+
+        df.to_csv(output_filepath, index=False)
