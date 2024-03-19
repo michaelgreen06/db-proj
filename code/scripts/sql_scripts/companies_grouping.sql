3587816b1a77ddecb10e68392d41b869f37a5afe
Michael Green <michaelgreen06@gmail.com>
2024-03-19 20:23:26 +0700

    working on logic for companies table"

--- code/scripts/sql_scripts/companies_grouping.sql
+++ code/scripts/sql_scripts/companies_grouping.sql
@@ -1,3 +1,4 @@
+--list prospect_ids that have names with matching 1st 3 words
 SELECT
   LOWER(
     TRIM(SPLIT_PART(name, ' ', 1)) || ' ' ||
@@ -12,6 +13,35 @@ GROUP BY
 ORDER BY
   first_three_words;
 
+--create companies table and fill it based on email_id
+
+-- Assuming the companies table has been created as follows:
+CREATE TABLE companies (
+    company_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email_id INTEGER UNIQUE,
+    CONSTRAINT fk_unique_email
+        FOREIGN KEY(email_id) 
+	        REFERENCES unique_emails(email_id)
+);
+
+-- Insert entries for email_ids associated with multiple prospect_ids
+INSERT INTO companies (name, email_id)
+SELECT LEFT(STRING_AGG(p.name, '; '), 100) AS company_name, pel.email_id
+FROM prospect_email_link pel
+JOIN prospects p ON pel.prospect_id = p.prospect_id
+GROUP BY pel.email_id
+HAVING COUNT(pel.prospect_id) > 1;
+
+-- Insert entries for email_ids associated with a single prospect_id
+INSERT INTO companies (name, email_id)
+SELECT p.name, pel.email_id
+FROM prospect_email_link pel
+JOIN prospects p ON pel.prospect_id = p.prospect_id
+LEFT JOIN companies c ON c.email_id = pel.email_id
+WHERE c.company_id IS NULL
+GROUP BY pel.email_id, p.name
+HAVING COUNT(pel.prospect_id) = 1;
 
 
 
